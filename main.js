53b5c134b7e0f2ccf8554aed6ba5e6e1e43d7b1f
AlenSkaria <hello.skaria@gmail.com>
2022-04-14 18:52:42 +0530

    v2.0

--- main.js
+++ main.js
@@ -23,6 +23,34 @@ t1.from(".whoami h4,.whoami h1,.passion h1,.passion h4,.btn-resume,.connect h1,.
 
 
 
+var t2 = gsap.timeline({
+    paused:"true"
+});
+t2.to(".menu",{
+    duration:1,
+    x:"0%",
+    ease: Expo.easeInOut
+});
+
+t2.fromTo(".li",{
+    y:"-100%",
+    opacity:0
+},{
+    duration:.4,
+    opacity:1,
+    y:"0%",
+    stagger:.25
+});
+
+
+function toggle (){
+    t2.play();
+}
+function togglec (){
+    t2.reverse();
+}
+
+
 
 
 
