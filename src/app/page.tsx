ff89480bcd277ec9e292e388c116875a445e69a0
AlenSkaria <hello.skaria@gmail.com>
2025-04-23 10:58:18 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { ThemeToggle } from "./theme-toggle";
-
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black text-black dark:text-white transition-all duration-300">
       <div className="text-center space-y-10">
         <div>skaria andrews</div>
-        
       </div>
     </div>
   );
